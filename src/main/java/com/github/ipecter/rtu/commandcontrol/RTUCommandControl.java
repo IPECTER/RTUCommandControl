7f8e1afb8e2e394c910b54f8fd5cc9ba261155c7
IPECTER <ipectert@gmail.com>
2022-07-11 19:44:29 +0900

    [ START DEV ] RTUCommandControl 1.0.0 Starting Development!

---
+++ src/main/java/com/github/ipecter/rtu/commandcontrol/RTUCommandControl.java
@@ -0,0 +1,17 @@
+package com.github.ipecter.rtu.commandcontrol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RTUCommandControl extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
