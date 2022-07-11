ea62a9f6d0d84ea8f5f39b316e088aac1476be11
IPECTER <ipectert@gmail.com>
2022-07-11 22:06:08 +0900

    [ EDIT ] RTUCommandControl 1.0.0

--- src/main/java/com/github/ipecter/rtu/commandcontrol/RTUCommandControl.java
+++ src/main/java/com/github/ipecter/rtu/commandcontrol/RTUCommandControl.java
@@ -1,17 +1,21 @@
 package com.github.ipecter.rtu.commandcontrol;
 
+import com.iridium.iridiumcolorapi.IridiumColorAPI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RTUCommandControl extends JavaPlugin {
 
+    public static final String prefix = IridiumColorAPI.process("<GRADIENT:fcf003>[ RTUCommandControl ]</GRADIENT:a3a3a3>");
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', prefix + " &aEnable&f!"));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info(ChatColor.translateAlternateColorCodes('&', prefix + " &cDisable&f!"));
     }
 }
